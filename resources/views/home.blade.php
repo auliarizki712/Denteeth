acd12799bda48bc2d5714a945cee19b302e047dc
Intan Malahayati <intan31@mhs.unsyiah.ac.id>
2021-11-22 20:20:31 +0700

    laravel

---
+++ resources/views/home.blade.php
@@ -0,0 +1,23 @@
+@extends('template/temp')
+
+@section('title', 'Home | Denteeth')
+
+
+@section('container')
+<section class="page-section text-dark">
+  <div class="row">
+    <div class="col text-start">
+      <h1>High-quality and<br>Painless dentistry</h1>
+    </div>
+    <div class="col text-center">
+      <div class="mt-5">
+        <a href="/sertifikat">
+          <div class="mb-2"><img src="https://img.icons8.com/color/48/000000/certification.png" height="60"/></div>
+          <h3 class="text-muted h4 mb-2">Sertifikat</h3>
+        </a>
+      </div>
+    </div>
+  </div>
+</section>
+<!-- button get started-->
+@endsection
